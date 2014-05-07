cce310207e5d08e6d030bc47c638214e61f8b5e2
gridmaster <antlerhorse@gmail.com>
2014-05-06 23:15:09 -0400

    working on DateTime issue...

--- SymbolService/Models/Sector.cs
+++ SymbolService/Models/Sector.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace SymbolService.Models
@@ -9,28 +7,28 @@ namespace SymbolService.Models
     {
         [JsonProperty(PropertyName = "Id")]
         public int Id { get; set; }
-        [JsonProperty(PropertyName = "Date")]
+        [JsonProperty(PropertyName = "date")]
         public DateTime Date { get; set; }
-        [JsonProperty(PropertyName = "Name")]
-        public string Name { get; set; }
-        [JsonProperty(PropertyName = "OneDayPriceChgPerCent")]
-        public decimal OneDayPriceChgPerCent { get; set; }
-        [JsonProperty(PropertyName = "MarketCap")]
-        public string MarketCap { get; set; }
-        [JsonProperty(PropertyName = "PriceToEarnings")]
-        public decimal PriceToEarnings { get; set; }
-        [JsonProperty(PropertyName = "ROEPerCent")]
-        public decimal ROEPerCent { get; set; }
-        [JsonProperty(PropertyName = "DivYieldPerCent")]
-        public decimal DivYieldPerCent { get; set; }
-        [JsonProperty(PropertyName = "DebtToEquity")]
-        public decimal DebtToEquity { get; set; }
-        [JsonProperty(PropertyName = "PriceToBook")]
-        public decimal PriceToBook { get; set; }
-        [JsonProperty(PropertyName = "NetProfitMarginMrq")]
-        public decimal NetProfitMarginMrq { get; set; }
-        [JsonProperty(PropertyName = "PriceToFreeCashFlowMrq")]
-        public decimal PriceToFreeCashFlowMrq { get; set; }
+        //[JsonProperty(PropertyName = "Name")]
+        //public string Name { get; set; }
+        //[JsonProperty(PropertyName = "OneDayPriceChgPerCent")]
+        //public decimal OneDayPriceChgPerCent { get; set; }
+        //[JsonProperty(PropertyName = "MarketCap")]
+        //public string MarketCap { get; set; }
+        //[JsonProperty(PropertyName = "PriceToEarnings")]
+        //public decimal PriceToEarnings { get; set; }
+        //[JsonProperty(PropertyName = "ROEPerCent")]
+        //public decimal ROEPerCent { get; set; }
+        //[JsonProperty(PropertyName = "DivYieldPerCent")]
+        //public decimal DivYieldPerCent { get; set; }
+        //[JsonProperty(PropertyName = "DebtToEquity")]
+        //public decimal DebtToEquity { get; set; }
+        //[JsonProperty(PropertyName = "PriceToBook")]
+        //public decimal PriceToBook { get; set; }
+        //[JsonProperty(PropertyName = "NetProfitMarginMrq")]
+        //public decimal NetProfitMarginMrq { get; set; }
+        //[JsonProperty(PropertyName = "PriceToFreeCashFlowMrq")]
+        //public decimal PriceToFreeCashFlowMrq { get; set; }
         //[Key]
         //[JsonProperty(PropertyName = "id")]
         //public int id { get; set; }
